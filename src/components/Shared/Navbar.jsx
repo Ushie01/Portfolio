ac0005cf32e2203eb3e4b02ae2f70360df612ae2
Ushie01 <abugodwinaj@gmail.com>
2023-07-22 04:23:34 +0100

    create: menu for mobile view

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Class, LINKS, SOCIAL_LINKS } from '../constant/Data';
 import useDeviceType from './useDeviceType';
 import Logo from './Logo';
 import Menu from '../../components/assets/list.svg';
+import Cancel from '../../components/assets/x-lg.svg';
 
 // Reusable Social Links Component
 const SocialLinks = () => (
@@ -23,6 +24,12 @@ const SocialLinks = () => (
 
 const Navbar = () => {
 	const { isMobile } = useDeviceType();
+	const [click, setClick] = useState(false);
+
+	const handleClick = () => {
+		console.log('hello')
+		setClick(!click);
+	};
 
 	if (isMobile) {
 		return (
@@ -33,10 +40,30 @@ const Navbar = () => {
 						src={Menu}
 						alt='Menu alt'
 						className='h-8 w-8'
+						onClick={handleClick}
 					/>
 				</div>
 				<SocialLinks />
 				<hr className='border w-full my-2' />
+				{click && (
+					<div className='fixed top-0 left-0 w-full z-20 h-full text-3xl text-white bg-black/50 '>
+						<img
+							src={Cancel}
+							alt='Cancel alt'
+							className='fixed top-6 right-3'
+							onClick={handleClick}
+						/>
+						<div className='flex flex-col items-center justify-center space-y-9 mt-44 '>
+							{LINKS.map((link, index) => (
+								<Link
+									key={index}
+									to={link.to}>
+									{link.text}
+								</Link>
+							))}
+						</div>
+					</div>
+				)}
 			</div>
 		);
 	}
